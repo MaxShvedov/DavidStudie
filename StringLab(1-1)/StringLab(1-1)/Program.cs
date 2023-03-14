cef9e1ea268669f5e1bc8df92df5a23150955c70
VIDgital <max.shvedov.tomsk@gmail.com>
2023-03-15 00:04:47 +0700

    Сделал 1-1

---
+++ StringLab(1-1)/StringLab(1-1)/Program.cs
@@ -0,0 +1,43 @@
+﻿namespace programm
+{
+    //TODO: Удалить комментарии кроме summary
+    class Program
+    {
+        /// <summary>
+        /// Возвращает значение True, если строка является палиндромом
+        /// </summary>
+        static bool IsInputSymmetric(string input)
+        {
+            int stringLength = input.Length; //Засунул в отдельную переменную на всякий случай
+
+            for (int i = 0; i < stringLength; i++) //Перебор строки
+            {
+                if (input[i] != input[stringLength - 1 - i]) //Сравнивает противоположные по позиции символы
+                {
+                    return false; //Если он не находит совпадения, метод сразу возвращает False, цикл прерывается
+                }
+            }
+
+            return true;
+        }
+
+        static void Main(string[] args)
+        {
+            int exitValue = 1;
+
+            while (exitValue != 0)
+            {
+                if (exitValue == 1) {
+                    Console.Write("Введите строку для проверки: ");
+
+                    string inputString = Console.ReadLine().ToLower();
+                    exitValue = -1;
+
+                    Console.WriteLine("Результат: {0}", IsInputSymmetric(inputString));
+                }
+                Console.WriteLine("Для продолжения введите 1.\nДля выхода из программы введите 0.");
+                exitValue = int.Parse(Console.ReadLine());
+            }
+        }
+    }
+}
