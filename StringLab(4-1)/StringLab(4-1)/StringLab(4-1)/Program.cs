3fc1f5026b7349bce60892369ce04b5e26bd2ae9
VIDgital <max.shvedov.tomsk@gmail.com>
2023-03-15 01:09:09 +0700

    Никита решил 4-1 :-\

---
+++ StringLab(4-1)/StringLab(4-1)/StringLab(4-1)/Program.cs
@@ -0,0 +1,18 @@
+﻿namespace program
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Введите занчение S: ");
+            string s = Console.ReadLine();
+            Console.Write("Введите значение S1: ");
+            string s1 = Console.ReadLine();
+            Console.Write("Введите значение S2: ");
+            string s2 = Console.ReadLine();
+
+            s = s.Remove(s.LastIndexOf(s1), s1.Length).Insert(s.LastIndexOf(s1), s2);
+            Console.WriteLine("Результат: {0}",s);
+        }
+    }
+}
