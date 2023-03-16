89d6302f8f8e3a8afc5c1f33793021a886493c75
VIDgital <max.shvedov.tomsk@gmail.com>
2023-03-16 16:42:03 +0700

    Сделал 3-1

---
+++ StringLab(3-1)/StringLab(3-1)/Program.cs
@@ -0,0 +1,33 @@
+﻿namespace program;
+class Program
+{
+    /// <summary>
+    /// Выбирает из массива типа string строки указанной длины и возвращает их как единую строку с разделителями
+    /// </summary>
+    static string SearchWordsInArray(string[] text, int length)
+    {
+        string words = "";
+
+        foreach (string i in text)
+        {
+            if (i.Length <= length)
+                words += i + "\n";
+        }
+        return words;
+    }
+
+    static void Main(string[] args)
+    {
+        int wordLenght;
+        string text;
+        string[] words;
+
+        Console.WriteLine("Введите текст:");
+        text = Console.ReadLine();
+        Console.Write("Введите максимальную длину искомых слов: ");
+        wordLenght = int.Parse(Console.ReadLine());
+        words = text.Split(new char[] { ' ', ',', '.' }, StringSplitOptions.RemoveEmptyEntries); //Второй параметр удаляет все пустые подстроки
+        text = SearchWordsInArray(words, wordLenght);
+        Console.Write("Найдены следующие слова:\n{0}", text);
+    }
+}
